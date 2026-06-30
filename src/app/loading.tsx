53f30ed111b91874dd772267dceeb3932715ba01
Krishna Kant <krshhh6@gmail.com>
2026-06-30 18:45:55 +0530

    feat: add global spinning text loader for page transitions

---
+++ src/app/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <div className="page-loader-card">
+      <div className="page-loader">
+        <p>loading</p>
+        <div className="page-loader-words">
+          <span className="page-loader-word">diagnostics</span>
+          <span className="page-loader-word">servicing</span>
+          <span className="page-loader-word">repairs</span>
+          <span className="page-loader-word">car wash</span>
+          <span className="page-loader-word">diagnostics</span>
+        </div>
+      </div>
+    </div>
+  );
+}
